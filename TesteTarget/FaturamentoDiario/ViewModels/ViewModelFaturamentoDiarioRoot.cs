3cd56a578df0ee8e84a3f7afc272551fe9f5ae8c
fdfcosta-telecon <fabricio.costa@teleconsistemas.com.br>
2023-03-28 09:05:56 -0300

    removendo viewmodel inutilizada

--- TesteTarget/FaturamentoDiario/ViewModels/ViewModelFaturamentoDiarioRoot.cs
+++
@@ -1,8 +0,0 @@
-﻿
-namespace FaturamentoDiario.ViewModels
-{
-    public class ViewModelFaturamentoDiarioRoot
-    {
-        public List<ViewModelFaturamentoDiario> ListaFaturamentoDiario { get; set; }
-    }
-}
